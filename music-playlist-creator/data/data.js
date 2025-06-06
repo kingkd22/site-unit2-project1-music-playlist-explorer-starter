e7350b8f977d8eb637bf93bc5b93526763303c3a
Kingston Davies <kingstond22@meta.com>
2025-06-06 09:50:59 -0700

    commit for render

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,546 @@
+const playlists = [
+    {
+        "playlistId": 1,
+        "playlistTitle": "Late Night Drive",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Mickey Mouse",
+        "playlistLikeCount": 0,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Back in Black",
+            "songArtist": "AC/DC",
+            "songAlbum": "Back in Black",
+            "songDuration": "4:15",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 2,
+            "songTitle": "Hotel California",
+            "songArtist": "Eagles",
+            "songAlbum": "Hotel California",
+            "songDuration": "6:30",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 3,
+            "songTitle": "Livin' on a Prayer",
+            "songArtist": "Bon Jovi",
+            "songAlbum": "Slippery When Wet",
+            "songDuration": "4:09",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 4,
+            "songTitle": "Smoke on the Water",
+            "songArtist": "Deep Purple",
+            "songAlbum": "Machine Head",
+            "songDuration": "5:40",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 5,
+            "songTitle": "Eye of the Tiger",
+            "songArtist": "Survivor",
+            "songAlbum": "Eye of the Tiger",
+            "songDuration": "4:05",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 6,
+            "songTitle": "Highway to Hell",
+            "songArtist": "AC/DC",
+            "songAlbum": "Highway to Hell",
+            "songDuration": "3:28",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        }
+        ]
+    },
+    {
+        "playlistId": 2,
+        "playlistTitle": "Favorite Songs",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Donald Duck",
+        "playlistLikeCount": 15,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Bohemian Rhapsody",
+            "songArtist": "Queen",
+            "songAlbum": "A Night at the Opera",
+            "songDuration": "5:55",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 2,
+            "songTitle": "Don't Stop Believin'",
+            "songArtist": "Journey",
+            "songAlbum": "Escape",
+            "songDuration": "4:11",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 3,
+            "songTitle": "Africa",
+            "songArtist": "Toto",
+            "songAlbum": "Toto IV",
+            "songDuration": "4:55",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 4,
+            "songTitle": "Take On Me",
+            "songArtist": "A-ha",
+            "songAlbum": "Hunting High and Low",
+            "songDuration": "3:47",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 5,
+            "songTitle": "Under Pressure",
+            "songArtist": "Queen & David Bowie",
+            "songAlbum": "Hot Space",
+            "songDuration": "4:08",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 6,
+            "songTitle": "Faithfully",
+            "songArtist": "Journey",
+            "songAlbum": "Frontiers",
+            "songDuration": "4:24",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        }
+        ]
+    },
+    {
+        "playlistId": 3,
+        "playlistTitle": "That Summer Feeling",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Goofy",
+        "playlistLikeCount": 22,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Imagine",
+            "songArtist": "John Lennon",
+            "songAlbum": "Imagine",
+            "songDuration": "3:01",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 2,
+            "songTitle": "Let It Be",
+            "songArtist": "The Beatles",
+            "songAlbum": "Let It Be",
+            "songDuration": "4:03",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 3,
+            "songTitle": "Hey Jude",
+            "songArtist": "The Beatles",
+            "songAlbum": "Single",
+            "songDuration": "7:08",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 4,
+            "songTitle": "Yesterday",
+            "songArtist": "The Beatles",
+            "songAlbum": "Help!",
+            "songDuration": "2:05",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 5,
+            "songTitle": "Piano Man",
+            "songArtist": "Billy Joel",
+            "songAlbum": "Piano Man",
+            "songDuration": "5:37",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 6,
+            "songTitle": "Tiny Dancer",
+            "songArtist": "Elton John",
+            "songAlbum": "Madman Across the Water",
+            "songDuration": "6:15",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 7,
+            "songTitle": "Rocket Man",
+            "songArtist": "Elton John",
+            "songAlbum": "Honky Château",
+            "songDuration": "4:41",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 8,
+            "songTitle": "Let Her Go",
+            "songArtist": "Passenger",
+            "songAlbum": "All the Little Lights",
+            "songDuration": "4:12",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        }
+        ]
+    },
+    {
+        "playlistId": 4,
+        "playlistTitle": "Workout Beats",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Daisy Duck",
+        "playlistLikeCount": 5,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Rolling in the Deep",
+            "songArtist": "Adele",
+            "songAlbum": "21",
+            "songDuration": "3:48",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 2,
+            "songTitle": "Someone Like You",
+            "songArtist": "Adele",
+            "songAlbum": "21",
+            "songDuration": "4:45",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 3,
+            "songTitle": "Halo",
+            "songArtist": "Beyoncé",
+            "songAlbum": "I Am... Sasha Fierce",
+            "songDuration": "4:21",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 4,
+            "songTitle": "Chandelier",
+            "songArtist": "Sia",
+            "songAlbum": "1000 Forms of Fear",
+            "songDuration": "3:36",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 5,
+            "songTitle": "Shallow",
+            "songArtist": "Lady Gaga & Bradley Cooper",
+            "songAlbum": "A Star is Born",
+            "songDuration": "3:37",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 6,
+            "songTitle": "Skyfall",
+            "songArtist": "Adele",
+            "songAlbum": "Skyfall (Single)",
+            "songDuration": "4:46",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 7,
+            "songTitle": "Royals",
+            "songArtist": "Lorde",
+            "songAlbum": "Pure Heroine",
+            "songDuration": "3:10",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 8,
+            "songTitle": "Stay",
+            "songArtist": "Rihanna",
+            "songAlbum": "Unapologetic",
+            "songDuration": "4:00",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 9,
+            "songTitle": "Set Fire to the Rain",
+            "songArtist": "Adele",
+            "songAlbum": "21",
+            "songDuration": "4:02",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        },
+        {
+            "songId": 10,
+            "songTitle": "Hello",
+            "songArtist": "Adele",
+            "songAlbum": "25",
+            "songDuration": "4:55",
+            "songImage": "https://picsum.photos/seed/picsum/200/300"
+        }
+        ]
+    },
+    {
+        "playlistId": 5,
+        "playlistTitle": "Road Trip Anthems",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Pluto",
+        "playlistLikeCount": 10,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Clocks",
+            "songArtist": "Coldplay",
+            "songAlbum": "A Rush of Blood to the Head",
+            "songDuration": "5:07",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 2,
+            "songTitle": "The Scientist",
+            "songArtist": "Coldplay",
+            "songAlbum": "A Rush of Blood to the Head",
+            "songDuration": "5:09",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 3,
+            "songTitle": "Fix You",
+            "songArtist": "Coldplay",
+            "songAlbum": "X&Y",
+            "songDuration": "4:55",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 4,
+            "songTitle": "Viva La Vida",
+            "songArtist": "Coldplay",
+            "songAlbum": "Viva la Vida",
+            "songDuration": "4:02",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 5,
+            "songTitle": "Yellow",
+            "songArtist": "Coldplay",
+            "songAlbum": "Parachutes",
+            "songDuration": "4:29",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 6,
+            "songTitle": "Paradise",
+            "songArtist": "Coldplay",
+            "songAlbum": "Mylo Xyloto",
+            "songDuration": "4:38",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 7,
+            "songTitle": "Adventure of a Lifetime",
+            "songArtist": "Coldplay",
+            "songAlbum": "A Head Full of Dreams",
+            "songDuration": "4:23",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 8,
+            "songTitle": "Speed of Sound",
+            "songArtist": "Coldplay",
+            "songAlbum": "X&Y",
+            "songDuration": "4:48",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        }
+        ]
+    },
+    {
+        "playlistId": 6,
+        "playlistTitle": "Boss Chick Energy",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Scrooge McDuck",
+        "playlistLikeCount": 50,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Money",
+            "songArtist": "Pink Floyd",
+            "songAlbum": "The Dark Side of the Moon",
+            "songDuration": "6:23",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 2,
+            "songTitle": "Rich Girl",
+            "songArtist": "Hall & Oates",
+            "songAlbum": "Bigger Than Both of Us",
+            "songDuration": "2:23",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 3,
+            "songTitle": "She Works Hard for the Money",
+            "songArtist": "Donna Summer",
+            "songAlbum": "She Works Hard for the Money",
+            "songDuration": "5:20",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 4,
+            "songTitle": "Easy Money",
+            "songArtist": "Billy Joel",
+            "songAlbum": "An Innocent Man",
+            "songDuration": "4:01",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 5,
+            "songTitle": "Take the Money and Run",
+            "songArtist": "Steve Miller Band",
+            "songAlbum": "Fly Like an Eagle",
+            "songDuration": "2:51",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 6,
+            "songTitle": "Opportunities (Let's Make Lots of Money)",
+            "songArtist": "Pet Shop Boys",
+            "songAlbum": "Please",
+            "songDuration": "3:45",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 7,
+            "songTitle": "If I Had $1,000,000",
+            "songArtist": "Barenaked Ladies",
+            "songAlbum": "Gordon",
+            "songDuration": "4:27",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        }
+        ]
+    },
+    {
+        "playlistId": 7,
+        "playlistTitle": "Rainy Nights",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Huey Duck",
+        "playlistLikeCount": 8,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Thunderstruck",
+            "songArtist": "AC/DC",
+            "songAlbum": "The Razors Edge",
+            "songDuration": "4:52",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 2,
+            "songTitle": "Enter Sandman",
+            "songArtist": "Metallica",
+            "songAlbum": "Metallica",
+            "songDuration": "5:31",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 3,
+            "songTitle": "Smells Like Teen Spirit",
+            "songArtist": "Nirvana",
+            "songAlbum": "Nevermind",
+            "songDuration": "5:01",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 4,
+            "songTitle": "Basket Case",
+            "songArtist": "Green Day",
+            "songAlbum": "Dookie",
+            "songDuration": "3:01",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 5,
+            "songTitle": "My Hero",
+            "songArtist": "Foo Fighters",
+            "songAlbum": "The Colour and the Shape",
+            "songDuration": "4:20",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 6,
+            "songTitle": "The Pretender",
+            "songArtist": "Foo Fighters",
+            "songAlbum": "Echoes, Silence, Patience & Grace",
+            "songDuration": "4:29",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 7,
+            "songTitle": "Everlong",
+            "songArtist": "Foo Fighters",
+            "songAlbum": "The Colour and the Shape",
+            "songDuration": "4:10",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        }
+        ]
+    },
+    {
+        "playlistId": 8,
+        "playlistTitle": "Nothing But The Classics",
+        "playlistImage": "https://picsum.photos/id/237/200/300",
+        "playlistCreator": "Louie Duck",
+        "playlistLikeCount": 18,
+        "playlistSongs": [
+        {
+            "songId": 1,
+            "songTitle": "Blinding Lights",
+            "songArtist": "The Weeknd",
+            "songAlbum": "After Hours",
+            "songDuration": "3:20",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 2,
+            "songTitle": "Levitating",
+            "songArtist": "Dua Lipa",
+            "songAlbum": "Future Nostalgia",
+            "songDuration": "3:24",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 3,
+            "songTitle": "Peaches",
+            "songArtist": "Justin Bieber",
+            "songAlbum": "Justice",
+            "songDuration": "3:18",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 4,
+            "songTitle": "Watermelon Sugar",
+            "songArtist": "Harry Styles",
+            "songAlbum": "Fine Line",
+            "songDuration": "2:54",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 5,
+            "songTitle": "As It Was",
+            "songArtist": "Harry Styles",
+            "songAlbum": "Harry's House",
+            "songDuration": "2:47",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 6,
+            "songTitle": "Save Your Tears",
+            "songArtist": "The Weeknd",
+            "songAlbum": "After Hours",
+            "songDuration": "3:36",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        },
+        {
+            "songId": 7,
+            "songTitle": "Break My Heart",
+            "songArtist": "Dua Lipa",
+            "songAlbum": "Future Nostalgia",
+            "songDuration": "3:41",
+            "songImage": "https://picsum.photos/seed/picsum/200/300",
+        }
+        ]
+    }
+    ]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,194 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const playlistCards = document.querySelector('.playlist-cards')
 
+    playlistCards.innerHTML = '';
+
+    playlists.forEach(item =>{
+        playlistCards.innerHTML += `
+        <article class="playlistcard" data-id="${item.playlistId}">
+            <img src="${item.playlistImage}" alt="playlist" width="100%">
+            <h3 class="playlistTitle">${item.playlistTitle}</h3>
+            <p class="creatorName">${item.playlistCreator}</p>
+            <p class="likes">
+            <i class="fas fa-heart heart-icon" style="color: lightgrey;" data-id="${item.playlistId}></i>
+            ${item.playlistLikeCount}
+            </p>
+        </article>
+        `
+    })
+
+
+    document.querySelectorAll('.playlistcard').forEach(card => {
+        
+        card.addEventListener('click', () => {
+            console.log('data set idea' + parseInt(card.dataset.id))
+            const id = parseInt(card.dataset.id);
+            const playlistMatch = playlists.find(p => p.playlistId === id);
+            if (playlistMatch) {
+                console.log('found match')
+                console.log(playlistMatch)
+                modalFill(playlistMatch);
+                document.querySelector('.modal').classList.add('show');
+            }
+        })
+    })
+})
+    // document.addEventListener('click', (e) => {
+    //     if (e.target.classList.contains("heart-icon")) {
+    //         const id = parseInt(e.target.dataset.id)
+    //         const playlist = playlist.find(p => p.playlistId === id);
+
+    //         if (!playlist) return;
+
+    //         const countSpan = e.target.nextElementSibling;
+    //         const isLiked = e.target.classList.contains("liked");
+
+    //         if (isLiked) {
+    //             playlist.playlistLikeCount -= 1;
+    //             e.target.style.color = "lightgrey";
+    //             e.target.classList.remove("liked")
+    //         }   else {
+    //             playlist.playlistLikeCount += 1;
+    //             e.target.style.color = "red";
+    //             e.target.classList.add("liked");
+    //         }
+    //         countSpan.textContent = playlist.playlistLikeCount;
+    //     }
+    // })
+
+
+// const modal = document.querySelector('.modal')
+
+// cards.forEach(card =>{
+
+// openModel()
+// function openModel() {
+//     const cards = document.querySelectorAll('.playlistcard')
+//     const modal = document.querySelector('.model')
+
+//     cards.forEach(card => {
+//         card.addEventListener('click', () => {
+//             const id = parseInt(card.dataset.id)
+//             const playlistMatch = playlists.find( item => item.playlistId === id)
+//             modalFill(playlistMatch)
+//             modal.classList.add('show')
+//         })
+//     })
+//     modal.addEventListener('click', (e) => {
+//         if ( e.target === modal){
+//             modal.classList.remove('show')
+//         }
+//     })
+// }
+    // console.log(playlist)
+    // // playlists.forEach((playlist) => {
+    // //     if (card.index === playlist.index) {
+    // const modal = document.getElementsByClassName('.modal')
+    // document.getElementById('playlistImage').src = playlist.playlistImage;
+    // document.getElementById('playlistName').textContent = playlist.playlist_name;
+    // document.getElementById('playlistCreator').textContent = `Creator: ${playlist.playlistCreator}`;
+
+    // const songList = document.querySelector('.songs');
+    // songList.innerHTML = '';
+
+    // playlist.playlistSongs.forEach((song) => {
+    //     const songItem = document.createElement("div");
+
+function modalFill(data) {
+    const modalContent = document.querySelector('.modalContent')
+    modalContent.innerHTML = `
+
+        
+        <button class="shuffle" data-id="${data.playlistId}> Shuffle </button>
+
+        `
+
+    const shuffle = modalContent.querySelector(".shuffle")
+    // shuffle.addEventListener("click", () => {
+    //     const playlist = playlists.find(p => p.playlistId === data.playlistId);
+    //     if (!playlist) return;
+
+    //     for (let i = playlist.playlistSongs.length - 1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         [playlist.playlistSongs[i], playlist.playlistSongs[j]] = [playlist.playlistSongs[j], playlist.playlistSongs[i]];
+    //     }
+    //     const songSection = modalContent.querySelector(".songs");
+    //     songSection.innerHTML = renderSongs(playlist.playlistSongs);egtnctfbkcgdlekgtvnnhlckliuniunkfgijjnllvndbgjfvljbntvvkvlvgndfffivigettjkecnbueeiurffdfrijbrngh
+    // })
+
+    const modal = document.querySelector(".modal");
+    modalContent.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.classList.contains('close')) {
+            modal.classList.remove('show');
+        }
+    })
+}
+
+function renderSongs(songs) {
+    return songs.map(song => `
+        <div class="song">
+            <img src="${song.songImage}" alt="song" width="100px" class="song-image">
+            <div class="song-description">
+                <h4 id="songName">${song.songTitle}</h4>
+                <p id="songArtist">${song.songArtist}</p>
+                <p id="albumName">${song.songAlbum}</p>
+            </div>
+            <div id="songDuration">
+                <p>${song.songDuration}</p>
+            </div>
+        </div>
+    `).join('');
+}
+    // <section class="songs">
+        // <section class="modal-header">
+        //     <img id="playlistImage" src="${data.playlistImage}" alt="Playlist Cover" width="200px">
+        //     <article class="playlist_details">
+        //         <h2 id="playlistName">${data.playlistTitle}</h2>
+        //         <p id="playlistCreator">${data.playlistCreator}</p>
+        //     </article>
+
+    //     <section class="songs">
+    //         ${renderSongs(data.playlistSongs)}
+    //     </section>
+    // `;
+        // </section>
+    // songItem.innerHTML = `
+    //     <div class="song">
+    //         <div class='song-image'>
+    //             <img src="${songImage}" alt="song" width="25px">
+    //         </div>
+    //         <div class="song-description">
+    //             <h4 id="songName">${songTitle}</h4>
+    //             <p id="songArtist">${songArtist}</p>
+    //             <p id="albumName">${songAlbum}</p>
+    //         </div>
+
+    //         <div id="songDuration">
+    //             <p>${songDuration}
+    //         </div>
+    //     </div>
+    //     `
+    //         songList.appendChild(songItem)
+    //     }
+    // modal.style.display = "block"
+
+    // modal.addEventListener('click', (e) => {
+    //     if ( e.target === modal){
+    //         modal.classList.remove('show')
+    //     }
+    // })
+
+    // card.addEventListener('click', () =>{
+    //     modal.classList.add('show')
+    // })
+
+
+// card.addEventListener('click', () =>{
+//         openModel()
+
+
+// modal.addEventListener('click', (e) => {
+//     if ( e.target === modal){
+//         modal.classList.remove('show')
+//     }
+// })
